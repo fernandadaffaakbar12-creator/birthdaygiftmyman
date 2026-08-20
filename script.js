a3deed824599c90c4fc54cf47939d18b07847f5c
Fernanda Daffa <emailkamu@gmail.com>
2026-08-20 22:24:41 +0700

    COMMIT

--- script.js
+++ script.js
@@ -175,7 +175,7 @@
         const pinPopupClose = document.getElementById('pin-popup-close');
 
         // DEFAULT PIN: Silakan ubah angka ini jika ingin PIN lain
-        const SECRET_PIN = "2708";
+        const SECRET_PIN = "2906";
 
         let pinAttempt = 0;
         let popupTimeout = null;
@@ -186,7 +186,7 @@
                 // Percobaan pertama: tampilkan foto kucing
                 showCat: true,
                 emoji: '',
-                message: 'Masa tanggal ultah aku lupa?',
+                message: 'Masa tanggal jadian kita lupa?',
                 buttonText: 'Iya iya maaf 😭'
             },
             {
